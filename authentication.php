6b83a94a7406dd46f041d8ac55816197af5f6bbd
komal523 <85786219+komal523@users.noreply.github.com>
2021-08-20 08:58:54 +0530

    first commit

---
+++ authentication.php
@@ -0,0 +1,25 @@
+    <?php      
+        include('connection.php');  
+        $username = $_GET['user'];  
+        $password = $_GET['pass'];  
+          
+            //to prevent from mysqli injection  
+            $username = stripcslashes($username);  
+            $password = stripcslashes($password);  
+            $username = mysqli_real_escape_string($con, $username);  
+            $password = mysqli_real_escape_string($con, $password);  
+          
+            $sql = "select *from customer where username = '$username' and password = '$password'";  
+            $result = mysqli_query($con, $sql);  
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+            $count = mysqli_num_rows($result);  
+              
+            if($count == 1)
+			{  
+                echo '<script>alert(" Login successful")</script>';  
+            }  
+            else
+			{  
+                echo '<script>alert("Login failed. Invalid username or password")</script>'; 
+            }     
+    ?>  
